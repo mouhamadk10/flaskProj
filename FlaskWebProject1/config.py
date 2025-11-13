1c19c864863d3b2d4c09bf465fe879dc7b523e7e
mouhamad <kaddourmouhamad86@gmail.com>
2025-11-13 15:21:08 +0100

    Initial commit

---
+++ FlaskWebProject1/config.py
@@ -0,0 +1,10 @@
+import os
+
+POSTGRES_HOST = os.getenv("POSTGRES_HOST", "db")
+POSTGRES_DB = os.getenv("POSTGRES_DB", "appdb")
+POSTGRES_USER = os.getenv("POSTGRES_USER", "appuser")
+POSTGRES_PASSWORD = os.getenv("POSTGRES_PASSWORD", "1234")
+
+REDIS_HOST = os.getenv("REDIS_HOST", "redis")
+REDIS_PORT = 6379
+
---
+++ FlaskWebProject1/views.py
@@ -0,0 +1,67 @@
+"""
+Routes and views for the flask application.
+"""
+
+from datetime import datetime
+from flask import render_template
+from FlaskWebProject1 import app
+import psycopg2
+import redis
+from FlaskWebProject1 import config
+
+redis_client = redis.StrictRedis(host=config.REDIS_HOST, port=config.REDIS_PORT, decode_responses=True)
+
+@app.route('/')
+@app.route('/home')
+def home():
+    """Renders the home page."""
+    return render_template(
+        'index.html',
+        title='Home Page',
+        year=datetime.now().year,
+    )
+
+@app.route('/contact')
+def contact():
+    """Renders the contact page."""
+    return render_template(
+        'contact.html',
+        title='Contact',
+        year=datetime.now().year,
+        message='Your contact page.'
+    )
+
+@app.route('/about')
+def about():
+    """Renders the about page."""
+    return render_template(
+        'about.html',
+        title='About',
+        year=datetime.now().year,
+        message='Your application description page.'
+    )
+
+@app.route('/db-test')
+def db_test():
+    try:
+        conn = psycopg2.connect(
+            host=config.POSTGRES_HOST,
+            database=config.POSTGRES_DB,
+            user=config.POSTGRES_USER,
+            password=config.POSTGRES_PASSWORD
+        )
+        cur = conn.cursor()
+        cur.execute("SELECT NOW();")
+        result = cur.fetchone()
+        conn.close()
+        return f"Postgres Connected — Server Time: {result}"
+    except Exception as e:
+        return f"Postgres {str(e)}"
+
+@app.route('/cache-test')
+def cache_test():
+    try:
+        count = redis_client.incr("visits")
+        return f"Redis Connected - Visit Count: {count}"
+    except Exception as e:
+        return f"Redis {str(e)}"
